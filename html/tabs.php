9d6fba112369fadba0051a6e98f50f37f8a7a36a
Farid Zenon <farid@volcom.my>
2014-09-25 02:13:32 +0800

    upload from local drive

---
+++ html/tabs.php
@@ -0,0 +1,7 @@
+<div class="tabs">
+<ul>
+	<li<?php echo ($t == 'camps') ? ' class="active"' : ''; ?>><a href="index.php">Campaigns</a></li>
+	<li<?php echo ($t == 'acc') ? ' class="active"' : ''; ?>><a href="account.php">Account</a></li>
+</ul>
+</div>
+<div style="clear: both;"></div>
