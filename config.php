1a3042f729140a4fbe3f470ae3f2809348fa1257
Farid Zenon <farid@volcom.my>
2014-09-25 02:22:54 +0800

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$l = @mysql_connect ( "HOST" , "DB_USER" , "DB_PASS" ) or die("<h1>Error in connecting to database</h1>");
-@mysql_select_db( "DB_NAME" ) or die("<h1>Error in connecting to database</h1>");
-
-$siteurl = 'SITE_URL'; // Enter the exact destination where the script will be used.  include http://
-
-?>
